80e375fc0302f671e09ac0aad9d3cdb6e9eb9521
Hitesh Sharma <hitesh.sharma@aqlix.com>
2023-12-12 11:01:18 +0530

    added about section and wokred on responsiveness

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './components/nav/Nav';
 import HeroSection from './components/hero-section/HeroSection';
 import Services from './components/services/Services';
 import NewNav from './components/newNav/NewNav';
+import About from './components/About/About';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
   <Nav/>
   <HeroSection/>
   <Services/>
+  <About/>
   </>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './About.css';
+import aboutlg from '../../assests/about-large.jpeg';
+import aboutsm from '../../assests/about-small..png';
+function About() {
+  return (
+    <>
+    <div className="about-section">
+        <div className="about-img">
+            <div className="about-lg">
+                <img src={aboutlg} alt="" />
+            </div>
+            <div className="about-sm">
+                <img src={aboutsm} alt="" />
+            </div>
+        </div>
+        <div className="about-desc">
+          <h1>About Onboarding-Mate</h1>
+          <p>Welcome to Onboarding Mate, your common platform and trusted companion for all individuals who are new to Germany and seeking assistance with various aspects of settling down in a foreign land. We understand that the challenges of navigating a new country, especially without fluency in the local language, can be overwhelming.</p>
+          <button className="read-more">READ MORE</button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default About
