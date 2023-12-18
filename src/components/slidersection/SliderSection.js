be71ba1eb6b1f42e2872cd46f5abc2ed40b398e9
Hitesh Sharma <hitesh.sharma@aqlix.com>
2023-12-18 13:33:30 +0530

    final phase

--- src/components/slidersection/SliderSection.js
+++ src/components/slidersection/SliderSection.js
@@ -3,18 +3,20 @@ import './slider.css';
 import family from '../../assests/family-and-pets.jpg';
 import Hi from '../../assests/home_insurance.2c0bd5282a4a47e2ed4d.jpg';
 import mi from '../../assests/medical_insurance.a008b389ed02e5addef2.jpg';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-// import Slider from "react-slick";
-// var settings = {
-//   dots: true,
-//   infinite: true,
-//   speed: 500,
-//   slidesToShow: 1,
-//   slidesToScroll: 1
-// };
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const sliderSettings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+};
+
 function SliderSection() {
-  const slider_data =[{
+  let slider_data =[{
     id:1,
     title:'Family & Pet',
     desc:'Get your family and pets insured and safeguarded by us even before traveling to the new country. Choose us so that you have teh process simplified',
@@ -32,33 +34,21 @@ function SliderSection() {
     desc:'Stay healthy and protected with our specialized health insurance for immigrants. Access quality care and enjoy peace of mind.',
     img:mi
   },
-  ]
+  ];
   return (
-    <>
-
- <div className="slider">
-      <div className="slider-container">
-        <h2>Our Best Servicess</h2>
-        <div className="slides">
-          {
-            slider_data.map((data)=>{
-             return(
-            <div className="slide-card" id={`slider-${data.id}`}>
-                <div className="slide-card-image">
-                    <img src={data.img} alt="" />
-                     <div className="slider-card-content">
-                      <h4>{data.title}</h4>
-                      <p>{data.desc}</p>
-                </div>
-                </div>
-            </div>
-             )
-            })
-          }
+      <>
+       <div className="slider">
+          <div className="slider-container">
+          z <h2>Our Best Servicess</h2>
+          </div>
         </div>
-      </div>
+       <Slider {...sliderSettings}>
+      {slider_data.map((card) => (
+       
+      ))}
+    </Slider>
     </div>
-    </>
+      </>
   )
 }
 
